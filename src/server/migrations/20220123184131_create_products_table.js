8906300cb1918b5b942d66bb1c203cf820bf69dc
Infamou5 <oreal_b52@hotmail.com>
2022-01-23 22:25:07 +0100

    Added DB Products Table - with migration and seed
    2 new files : 20220123184131_create_products_table.js
                        01_products_table.js
    
    2 deleted files: modules.js
                            20200510015322_create_modules_schema.js

---
+++ src/server/migrations/20220123184131_create_products_table.js
@@ -0,0 +1,39 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('products', function (table) {
+    table.increments();
+    table.string('name').notNullable();
+    table.integer('price').notNullable();
+    table
+      .enum('color', [
+        'unknown',
+        'white',
+        'yellow',
+        'red',
+        'green',
+        'blue',
+        'purple',
+        'pink',
+        'orange',
+      ])
+      .nullable()
+      .defaultTo('unknown');
+    table
+      .enum('size', ['unknown', 's', 'sm', 'm', 'l', 'xl', 'xxl'])
+      .nullable()
+      .defaultTo('unknown');
+    table.boolean('is_available').nullable().defaultTo(false);
+    table.integer('stock_amount').nullable().defaultTo(0);
+    table.boolean('is_on_discount').nullable().defaultTo(false);
+    table.integer('discount_percent').nullable().defaultTo(0);
+    table
+      .string('picture')
+      .nullable()
+      .defaultTo('https://via.placeholder.com/500x500');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('products');
+};
