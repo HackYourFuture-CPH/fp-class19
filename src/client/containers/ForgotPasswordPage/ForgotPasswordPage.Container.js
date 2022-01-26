c2c87b254a3a3f73790dff347856f99b95dd68a2
FlorentinaPetica <florentina.petica@gmail.com>
2022-01-26 22:17:29 +0100

    Frontend: Dummy Pages

---
+++ src/client/containers/ForgotPasswordPage/ForgotPasswordPage.Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ForgotPasswordPage.Style.css';
+
+function ForgotPasswordPage() {
+  return <div>Forgot Password</div>;
+}
+
+export default ForgotPasswordPage;
---
+++ src/client/containers/SpecialOfferPage/SpecialOfferPage.Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SpecialOfferPage.Style.css';
+
+function SpecialOfferPage() {
+  return <div>Special Offers</div>;
+}
+
+export default SpecialOfferPage;
