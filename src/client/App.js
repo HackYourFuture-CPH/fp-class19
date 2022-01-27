5dfcfdc64c3fa731c2f045ec089e2be819d690d5
suvarna <suvarna.akkala@gmail.com>
2022-01-27 16:52:09 +0100

    Revert "Merge branch 'feature/loginform' of https://github.com/HackYourFuture-CPH/fp-class19 into feature/loginform"
    This reverts commit 5d7269242b9e4bdc11344bd6bb4b7966679cc990, reversing
    changes made to eceec231baa5b9c3b5513e0a92b58c9a2db0e83e.

--- src/client/App.js
+++ src/client/App.js
@@ -11,9 +11,6 @@ import Header from './components/Navigation/Header';
 import Profile from './containers/Profile';
 import Loader from './components/Loader';
 import LogIn from './components/LoginComponent/LoginComponent';
-import Header from './components/Navigation/Pagination';
-import Header from './components/Posts';
-import axios from 'axios';
 
 function App() {
   const { isLoading } = useAuthentication();
@@ -21,30 +18,6 @@ function App() {
   if (isLoading) {
     return <Loader />;
   }
-
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(10);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      setPosts(res.data);
-      setLoading(false);
-    };
-
-    fetchPosts();
-  }, []);
-
-  // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <Router>
@@ -66,13 +39,6 @@ function App() {
           <Profile />
         </AuthenticatedRoute>
       </Switch>
-
-      <Posts posts={currentPosts} loading={loading} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
-        paginate={paginate}
-      />
     </Router>
   );
 }
