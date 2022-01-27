7dea8a720ad77e37e47a40828a41441c7d4c59b2
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2022-01-27 17:53:28 +0100

    Added SpecialOfferBanner to Home.js file

--- src/client/containers/Home/Home.js
+++ src/client/containers/Home/Home.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import SpecialOfferBanner from '../../components/SpecialOfferBanner/Specialoffer';
 
-export const Home = () => <section className="home">Final project!</section>;
+export const Home = () => {
+  return (
+    <div>
+      <SpecialOfferBanner />
+      <section className="home">Final project!</section>
+    </div>
+  );
+};
