b5579169d03ee3515859299c6393fa4d33ecb2bc
Parul Jain <hparul@gmail.com>
2022-01-29 18:28:24 +0100

    updated all product files and images folder

--- src/client/components/Product/Product.js
+++ src/client/components/Product/Product.js
@@ -1,30 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Product.styles.css';
+import cart_bucket from './..\\..\\assets\\images\\cart_bucket.png';
+import heart from './..\\..\\assets\\images\\heart.png';
 
-export default function Product({ image, name, price, onClick }) {
+export default function Product({
+  image,
+  name,
+  price,
+  onClick,
+  addToFavorites,
+}) {
   return (
-    <div className="product-component">
+    <div className="product-container">
       <img className="product-image" src={image} />
-      <br></br>
+
       <h2 className="product-name">{name}</h2>
-      <br></br>
+
       <h2 className="product-price">{price}</h2>
-      <br></br>
-      <button className="product-add-button" type="button" onClick={onClick}>
-        ADD
-      </button>
+
+      <div className="add-fav-button">
+        <div>
+          <button
+            className="product-add-button"
+            type="button"
+            onClick={onClick}
+          >
+            ADD
+            <img src={cart_bucket} className="vector-bucket" />
+          </button>
+        </div>
+
+        <div className="fav-button-div">
+          <button
+            className="favorite-button"
+            type="button"
+            onClick={addToFavorites}
+          >
+            <img src={heart} className="favorite-button" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
 Product.propTypes = {
-  title: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
   onClick: PropTypes.func,
+  addToFavorites: PropTypes.func,
 };
 
 Product.defaultProps = {
   onClick: null,
+  addToFavorites: null,
 };
--- src/client/components/Product/Product.stories.js
+++ src/client/components/Product/Product.stories.js
@@ -4,20 +4,24 @@ import React from 'react';
 
 import Product from './Product';
 
+import imageFile from '..\\..\\assets\\images\\image 4.png';
+
 export default {
   title: 'Components / Product Component',
   component: Product,
   argTypes: {
     onClick: { action: 'clicked' },
+    addToFavorites: { action: 'clicked' },
   },
   parameters: { actions: { argTypesRegex: '^on.*' } },
 };
 
 export const Component = () => (
   <Product
-    image={text('Image', '../../assets/images/hyf.png')}
+    image={imageFile}
     name={text('Name', 'Test Image')}
     price={text('Price', '20 DKK')}
-    onClick={action('You have clicked the button')}
+    onClick={action('You have clicked the add to cart button')}
+    addToFavorites={action('You have clicked the add to favorites button')}
   />
 );
