cf52b84eb0fe2526191ed93bb0d94c3a75931fd9
Infamou5 <oreal_b52@hotmail.com>
2022-01-29 20:35:52 +0100

    Deleted unnecessary files. Changed .css file

--- src/client/components/Menu/Menu.js
+++ src/client/components/Menu/Menu.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import './menu.css';
-import menuItems from './menuItems';
+import { NavLink } from 'react-router-dom';
 
 export default function Menu() {
   return (
     <div className="menu">
       <ul className="menu-list">
-        {menuItems?.map((menuItem) => {
-          const { name, to, className, activeStyle } = menuItem;
-          return (
-            <li className="menu-list-item">
-              <NavLink to={to} className={className} activeStyle={activeStyle}>
-                {name}
-              </NavLink>
-            </li>
-          );
-        })}
+        <li className="menu-list-item">
+          <NavLink to="/plants" className="menu-list-item-link">
+            Plants
+          </NavLink>
+        </li>
+        <li className="menu-list-item">
+          <NavLink to="/special-offers" className="menu-list-item-link">
+            Special Offers
+          </NavLink>
+        </li>
+        <li className="menu-list-item">
+          <NavLink to="/about-us" className="menu-list-item-link">
+            About
+          </NavLink>
+        </li>
+        <li className="menu-list-item">
+          <NavLink to="/contact" className="menu-list-item-link">
+            Contact
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
