8e0267f4d612d023332ac989e23c08015bb01773
Infamou5 <oreal_b52@hotmail.com>
2022-01-30 19:03:51 +0100

    Implemented a loader that uses a Spinner.svg file
    Spinner takes the following props: scale and color.

--- src/client/components/Loader/index.js
+++ src/client/components/Loader/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import Spinner from './Spinner';
 
 export default function Loader() {
-  return <div>Loading...</div>;
+  return <Spinner scale={1.2} color="#687808" />;
 }
