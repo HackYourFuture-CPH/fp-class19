bf942a359381076248f3292dc58615bb651332d8
Divyajg <guttuladivyajyothi@gmail.com>
2022-01-30 17:17:34 +0100

    changed classnames to more appropriate

--- src/client/components/contact-page/Contact.js
+++ src/client/components/contact-page/Contact.js
@@ -4,45 +4,57 @@ import './contact.css';
 
 export default function ContactPage() {
   return (
-    <div className="page-margin">
-      <h1 className="title">HAPPY TO HELP</h1>
+    <div className="contact-margin">
+      <h1 className="contact-title">HAPPY TO HELP</h1>
       <div>
-        <h3 className="content">Comments and Criticism</h3>
-        <p className="content">
+        <h3 className="contact-content-h3">Comments and Criticism</h3>
+        <p className="contact-content-p">
           Your opinion matters to us. So if you think we can do something better
           or you have an idea that you think will help improve our offering,{' '}
           <br />
           let us know by emailing{' '}
-          <a href="mailto:customerservices@thegreenstore.dk">
+          <a
+            className="contact-alink"
+            href="mailto:customerservices@thegreenstore.dk"
+          >
             customerservices@thegreenstore.dk
           </a>
           .
         </p>
-        <h3 className="content">General queries</h3>
-        <p className="content">
+        <h3 className="contact-content-h3">General queries</h3>
+        <p className="contact-content-p">
           For all other queries, please email{' '}
-          <a href="mailto:customerservices@thegreenstore.dk">
+          <a
+            className="contact-alink"
+            href="mailto:customerservices@thegreenstore.dk"
+          >
             customerservices@thegreenstore.dk
           </a>
           .
         </p>
-        <p className="content">
+        <p className="contact-content-p">
           If your query is urgent, please call us on{' '}
-          <a href="tel:+4531523612">+4531523612</a>. Lines are open from 9am to
-          5pm, Monady - Friday
+          <a className="contact-alink" href="tel:+4531523612">
+            +4531523612
+          </a>
+          . Lines are open from 9am to 5pm, Monady - Friday
         </p>
-        <h3 className="content">Join our affiliate programme</h3>
-        <p className="content">
+        <h3 className="contact-content-h3">Join our affiliate programme</h3>
+        <p className="contact-content-p">
           Visit our affiliate page for further information.
         </p>
-        <h3 className="content">Press enquiries</h3>
-        <p className="content">Please see our latest press coverage here.</p>
-        <p className="content">
-          All medis enquiries should be emailed through to:{' '}
-          <a href="mailto:press@thegreenstore.dk">press@thegreenstore.dk</a>
+        <h3 className="contact-content-h3">Press enquiries</h3>
+        <p className="contact-content-p">
+          Please see our latest press coverage here.
         </p>
-        <h3 className="content">Head office address</h3>
-        <p className="content">
+        <p className="contact-content-p">
+          All medis enquiries should be emailed through to:{' '}
+          <a className="contact-alink" href="mailto:press@thegreenstore.dk">
+            press@thegreenstore.dk
+          </a>
+        </p>
+        <h3 className="contact-content-h3">Head office address</h3>
+        <p className="contact-content-p">
           The Green Store,
           <br />
           Nursery Court, <br /> Copenhagen 2102
@@ -51,6 +63,7 @@ export default function ContactPage() {
       <img
         src={image}
         alt="If you have a garden and a library, You have everything you need - MARCUS TULLIUS CICERO(HELLOHOMESTEAD.COM)"
+        className="contact-image"
       />
     </div>
   );
