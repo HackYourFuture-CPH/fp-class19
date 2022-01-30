43529dc6dfc92c3e803f0f79c305da2baedcee68
suvarna <suvarna.akkala@gmail.com>
2022-01-30 12:03:38 +0100

    initial commit

---
+++ src/client/components/ProductsComponent/ProductsComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './ProductsComponent.styles.css';
+// import Product from '../ProductComponent/ProductComponent';
+
+export default function ProductsComponent() {
+  return <div className="productsComponent"></div>;
+}
