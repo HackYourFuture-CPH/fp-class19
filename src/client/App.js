9786fd2dddd915aca30ec170632380921dc7b48c
Darya-s <darya.stepanova95@gmail.com>
2022-02-01 20:56:00 +0100

    Create App.js

---
+++ src/client/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Home } from './containers/Home/Home';
+import SignIn from './containers/SignIn';
+import SignUp from './containers/SignUp';
+import ResetPassword from './containers/ResetPassword';
+import AuthenticatedRoute from './components/Auth/AuthenticatedRoute';
+import { useAuthentication } from './hooks/useAuthentication';
+import { Header } from './components/Header/Header';
+import Profile from './containers/Profile';
+import Loader from './components/Loader';
+import LogIn from './components/LoginComponent/LoginComponent';
+import Menu from './components/Menu/Menu';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  const { isLoading } = useAuthentication();
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <Router>
+      <Header />
+      <Menu />
+      <Switch>
+        {/* Home page */}
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        {/* Anonymous pages */}
+        <SignIn exact path="/sign-in" />
+        <SignUp exact path="/sign-up" />
+        <ResetPassword exact path="/reset-password" />
+        <LogIn exact path="/log-in" />
+
+        {/* All routes below are authenticated routes - a user must login first */}
+        <AuthenticatedRoute exact path="/profile">
+          <Profile />
+        </AuthenticatedRoute>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
