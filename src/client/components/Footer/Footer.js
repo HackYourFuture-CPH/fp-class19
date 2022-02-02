ac7c1b766473e8ff679dd1f4e1510cf8b11a68c0
Jiu Tak <aurajiu@gmail.com>
2022-02-02 20:49:30 +0100

    styling changes

--- src/client/components/Footer/Footer.js
+++ src/client/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '*../../../src/client/assets/images/logo.png';
+import logo from '*../../../src/client/assets/images/Logo.png';
 import send from '*../../../src/client/assets/images/emailsend.png';
 import './Footer.css';
 import { Link } from 'react-router-dom';
@@ -14,25 +14,28 @@ function Footer() {
               <img src={logo} alt="logo" />
             </div>
           </li>
-          <li className="footer-list">
-            <div className="footer-text">
-              <h4>COMPANY</h4>
-              <Link to="/About">About us</Link>
-              <br />
-              <Link to="/contact-us">Contact us</Link>
-            </div>
-          </li>
-          <li className="footer-list">
-            <div className="footer-text">
-              <Link to="/sign-up">
-                <h4>CREATE ACCOUNT</h4>
-              </Link>
-              <div className="footer-input">
-                <input type="text" placeholder="Your email" />
-                <img src={send} alt="email" />
+
+          <ul className="right-part-footer">
+            <li className="footer-list">
+              <div className="footer-text">
+                <p>COMPANY</p>
+                <Link to="/About">About us</Link>
+                <br />
+                <Link to="/contact-us">Contact us</Link>
               </div>
-            </div>
-          </li>
+            </li>
+            <li className="footer-list">
+              <div className="footer-text">
+                <Link to="/sign-up">
+                  <p>CREATE ACCOUNT</p>
+                </Link>
+                <div className="footer-input">
+                  <input type="text" placeholder="Your email" />
+                  <img src={send} alt="email" />
+                </div>
+              </div>
+            </li>
+          </ul>
         </ul>
       </footer>
     </div>
