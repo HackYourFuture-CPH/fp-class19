40eaea69f17acff9aa8ee6cb2444ded29a14af48
suvarna <suvarna.akkala@gmail.com>
2022-02-03 20:37:07 +0100

    products-view

---
+++ src/client/components/ProductsView/ProductsView.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './ProductsView.styles.css';
+import Product from '../Product/Product';
+
+export default function ProductsView({ products }) {
+  const addFavorites = () => {
+    console.log('added to favorites');
+  };
+  const addToCart = () => {
+    console.log('added to cart');
+  };
+  return (
+    <div className="products-view">
+      <div>
+        <ul className="product-list">
+          {products.map((product) => (
+            <li key={product.id} className="product-item">
+              <Product
+                image={product.picture}
+                name={product.name}
+                price={product.price}
+                onClick={addToCart}
+                addToFavorites={addFavorites}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
