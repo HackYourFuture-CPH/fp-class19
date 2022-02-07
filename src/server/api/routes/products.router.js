52f52e140ba47d74d002c26a0f2e5187e36e63a7
FlorentinaPetica <florentina.petica@gmail.com>
2022-02-07 21:49:24 +0100

    Create additional query parameters for protucts api limit and offset

--- src/server/api/routes/products.router.js
+++ src/server/api/routes/products.router.js
@@ -23,7 +23,7 @@ const productsController = require('../controllers/products.controller');
  */
 router.get('/', (req, res, next) => {
   productsController
-    .getProducts()
+    .getProducts(req)
     .then((result) => res.json(result))
     .catch(next);
 });
