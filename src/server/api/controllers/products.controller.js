5ad23db907c3e1a8df280e65993a5b46b007102e
Divyajg <guttuladivyajyothi@gmail.com>
2022-02-07 15:12:06 +0100

    added a function to get product by id

--- src/server/api/controllers/products.controller.js
+++ src/server/api/controllers/products.controller.js
@@ -1,11 +1,27 @@
 const knex = require('../../config/db');
-// const HttpError = require('../lib/utils/http-error');
+const HttpError = require('../lib/utils/http-error');
 // const moment = require('moment-timezone');
 
 const getProducts = async () => {
   return knex('products');
 };
 
+const getProductById = async (id) => {
+  if (!id) {
+    throw new HttpError('Id should be a number', 400);
+  }
+
+  try {
+    const products = await knex('products').select('products.*').where({ id });
+    if (products.length === 0) {
+      throw new Error(`incorrect entry with the id of ${id}`, 404);
+    }
+    return products;
+  } catch (error) {
+    return error.message;
+  }
+};
 module.exports = {
   getProducts,
+  getProductById,
 };
