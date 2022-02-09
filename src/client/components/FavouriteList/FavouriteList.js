7c4a050a5ea41372c363dac85d8d9687a02ec9d8
Parul Jain <hparul@gmail.com>
2022-02-09 13:39:29 +0100

    updated favoritelist file

--- src/client/components/FavouriteList/FavouriteList.js
+++ src/client/components/FavouriteList/FavouriteList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './FavouriteList.styles.css';
 import ProductImage from './ProductImage.js';
