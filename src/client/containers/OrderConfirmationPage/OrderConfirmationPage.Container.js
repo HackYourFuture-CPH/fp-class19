9d90bbe46810e69c5f1c29280793db224af095fb
Darya-s <darya.stepanova95@gmail.com>
2022-02-09 20:38:33 +0100

    Update OrderConfirmationPage.Container.js

--- src/client/containers/OrderConfirmationPage/OrderConfirmationPage.Container.js
+++ src/client/containers/OrderConfirmationPage/OrderConfirmationPage.Container.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import './OrderConfirmationPage.Style.css';
+import ButtonComponent from '../../components/Button/Button';
 
 function OrderConfirmationPage() {
-  return <div>Order Confirmation Page</div>;
+  return <div className="confirmationPage">
+      <div className="wrapper">
+        <div>
+          <h3>ORDER CONFIRMATION</h3>
+        </div>
+        <div className="wrapper-content">
+          <p>
+            THANK YOU FOR YOUR ORDER
+          </p>
+          <p>
+            We have sent a receipt to the email
+          </p>
+        </div>
+      </div>
+      <div className="btn">
+        <ButtonComponent
+          label="CONTINUE SHOPPING"
+  primary=true
+          backgroundColor="#687808"
+          onClick={() => {
+            window.location.href = '/';
+          }}
+        />
+      </div>
+<div className="btn">
+        <ButtonComponent
+          label="REVIEW ORDER"
+          backgroundColor="#fff"
+          onClick={() => {
+            window.location.href = '/order';
+          }}
+        />
+      </div>
+    </div>;
 }
 
 export default OrderConfirmationPage;
