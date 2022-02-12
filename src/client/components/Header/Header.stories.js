0f66ff44204b46f681213814610749bcd32fcdd8
suvarna <suvarna.akkala@gmail.com>
2022-02-12 13:11:11 +0100

    error fix

--- src/client/components/Header/Header.stories.js
+++ src/client/components/Header/Header.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-
-import { Header } from './Header';
+import { Header } from './Header.js';
 
 export default { title: 'Components / Header' };
 
