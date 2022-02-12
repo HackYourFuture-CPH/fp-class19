a079510090cc682fc07d8e96d4c7a3d4b3377b0f
FlorentinaPetica <florentina.petica@gmail.com>
2022-02-12 17:39:31 +0100

    Components added to landing page container

--- src/client/containers/LandingPage/LandingPage.Container.js
+++ src/client/containers/LandingPage/LandingPage.Container.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LandingPage.Style.css';
 import { useProducts } from '../../hooks/useProducts';
-import ProductsComponent from '../../components/ProductsComponent/ProductsComponent';
 import Loader from '../../components/Loader/Loader.component';
-import { Header } from '../../components/Header/Header';
+import Header from '../../components/Header/Header';
 import Menu from '../../components/Menu/Menu';
 import SpecialOfferBanner from '../../components/SpecialOfferBanner/Specialoffer';
 import Footer from '../../components/Footer/Footer';
 import PaginationComponent from '../../components/PaginationComponent/PaginationComponent';
+import SortComponent from '../../components/SortComponent/SortComponent';
 
 function LandingPage() {
   const { products, isLoading, error } = useProducts();
+  const [sortedProducts, setSortedProducts] = useState(products);
 
   return (
-    <div>
+    <div className="landingPageContaier">
       <Header />
       <Menu />
       <SpecialOfferBanner />
       {isLoading && <Loader />}
       {!isLoading && !error && (
-        <div style={{ marginBottom: '22px' }}>
-          <ProductsComponent products={products} />{' '}
+        <div style={{ marginBottom: '25px' }}>
+          <SortComponent
+            products={products}
+            setSortedProducts={setSortedProducts}
+          />
           <PaginationComponent products={products} productsPerPage={6} />
         </div>
       )}
