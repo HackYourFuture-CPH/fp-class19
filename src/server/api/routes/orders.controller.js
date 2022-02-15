28cdbc1dfe2b5c86943dc3a6bff3bc2f59f684d9
Darya-s <darya.stepanova95@gmail.com>
2022-02-15 20:34:10 +0100

    Create orders.controller.js

---
+++ src/server/api/routes/orders.controller.js
@@ -0,0 +1,25 @@
+const knex = require('../../config/db');
+const moment = require('moment-timezone');
+const HttpError = require('../lib/utils/http-error');
+
+const newOrder = async (data) => {
+  let orderId;
+  await knex('orders')
+    .insert({ user_id: data.user_id })
+    .returning('id')
+    .then((id) => {
+      [orderId] = id;
+      data.items.forEach(async (item) => {
+        await knex('order_items').insert({
+          order_id: id[0],
+          product_id: item.productId,
+          quantity: item.quantity,
+        });
+      });
+    });
+  return orderId;
+};
+
+module.exports = {
+  newOrder
+};
