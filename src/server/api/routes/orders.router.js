999f4f1cdb19f24a52bdfa3d5e9e62cb67ee9768
Darya-s <darya.stepanova95@gmail.com>
2022-02-15 20:40:05 +0100

    Update orders.router.js

--- src/server/api/routes/orders.router.js
+++ src/server/api/routes/orders.router.js
@@ -5,6 +5,40 @@ const router = express.Router({ mergeParams: true });
 // controllers
 const ordersController = require('../controllers/orders.controller');
 
+/**
+ * @swagger
+ * /orders:
+ *  post:
+ *    tags:
+ *    - Orders
+ *    summary: Save new order information
+ *    description:
+ *     will save an order for a user
+ *    produces: application/json
+ *    parameters:
+ *      - in: body
+ *        name: Order
+ *        description: create a new order for a user
+ *        schema:
+ *          type: object
+ *          properties:
+ *            user_id:
+ *              type: string
+ *            items:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  productId:
+ *                    type: integer
+ *                  quantity:
+ *                    type: integer
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
 
 router.post('/', (req, res, next) => {
   ordersController
