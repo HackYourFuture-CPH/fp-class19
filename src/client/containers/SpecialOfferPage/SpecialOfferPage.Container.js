36b2fb9f59c4b781c3f65f6c9a66952483a440c3
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2022-02-18 14:29:02 +0100

    SpecialOffer Page created without component

--- src/client/containers/SpecialOfferPage/SpecialOfferPage.Container.js
+++ src/client/containers/SpecialOfferPage/SpecialOfferPage.Container.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Loader from '../../components/Loader/Loader.component';
 import './SpecialOfferPage.styles.css';
 
 export default function SpecialOfferPage() {
-  return <div>Special Offers</div>;
+  return (
+    <div>
+      Special Offers are Loading...
+      <Loader />
+    </div>
+  );
 }
