23195bffa2a0da5b6d6a1d513f6e41f47c42bf18
Infamou5 <oreal_b52@hotmail.com>
2022-02-19 20:19:27 +0100

    Added more entries to seed files

--- src/server/seeds/development/03_orders_table.js
+++ src/server/seeds/development/03_orders_table.js
@@ -1,30 +1,51 @@
 /* eslint-disable prefer-arrow-callback */
-exports.seed = function(knex) {
-    // Deletes ALL existing entries
-    return knex('orders')
-        .del()
-        .then(function() {
-            // Inserts seed entries
-            return knex('orders').insert([{
-                    user_id: 1,
-                    created_at: knex.fn.now(),
-                    status: 'NEW',
-                },
-                {
-                    user_id: 2,
-                    created_at: knex.fn.now(),
-                    status: 'INPROGRESS',
-                },
-                {
-                    user_id: 3,
-                    created_at: knex.fn.now(),
-                    status: 'CANCELLED',
-                },
-                {
-                    user_id: 4,
-                    created_at: knex.fn.now(),
-                    status: 'NEW',
-                },
-            ]);
-        });
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('orders')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('orders').insert([
+        {
+          user_id: 1,
+          created_at: knex.fn.now(),
+          status: 'NEW',
+        },
+        {
+          user_id: 2,
+          created_at: knex.fn.now(),
+          status: 'INPROGRESS',
+        },
+        {
+          user_id: 3,
+          created_at: knex.fn.now(),
+          status: 'CANCELLED',
+        },
+        {
+          user_id: 4,
+          created_at: knex.fn.now(),
+          status: 'NEW',
+        },
+        {
+          user_id: 1,
+          created_at: knex.fn.now(),
+          status: 'NEW',
+        },
+        {
+          user_id: 2,
+          created_at: knex.fn.now(),
+          status: 'NEW',
+        },
+        {
+          user_id: 3,
+          created_at: knex.fn.now(),
+          status: 'NEW',
+        },
+        {
+          user_id: 4,
+          created_at: knex.fn.now(),
+          status: 'NEW',
+        },
+      ]);
+    });
 };
