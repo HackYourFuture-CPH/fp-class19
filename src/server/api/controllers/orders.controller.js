8f25be6f2045411028f03f657755795ca38fd0fa
Natalia Sowa <natalia.sowaa@gmail.com>
2022-02-19 22:07:02 +0100

    fixes in select

--- src/server/api/controllers/orders.controller.js
+++ src/server/api/controllers/orders.controller.js
@@ -1,6 +1,7 @@
 const knex = require('../../config/db');
 const HttpError = require('../lib/utils/http-error');
 
+  
 const getOrderById = async(id) => {
     if (!id) {
         throw new HttpError(
@@ -10,35 +11,34 @@ const getOrderById = async(id) => {
     }
     try {
         const orders = await knex('orders AS o')
+        // .select('orders.id as id', 'status', 'quantity', 'name', etc..)
             .select(
-                'o.id as orderId',
-                'o.status as orderStatus',
-                'o.created_at as orderDate',
-                'o.user_id as userId',
-                'p.id as productId',
+                'o.id',
+                'o.status',
+                'o.created_at',
+                'o.user_id',
                 'oi.quantity',
+                'p.id',
                 'p.name',
-                'p.price',
-                'p.color',
-                'p.size',
                 'p.picture',
-                'p.stock_amount'
+                'p.stock_amount',
+                'p.price',
             )
-            .join('order_items AS oi ', 'o.id', '=', 'oi.order_id')
+            .join('order_items AS oi', 'o.id', '=', 'oi.order_id')
             .join('products AS p', 'p.id', '=', 'oi.product_id')
             .where('o.id', '=', id);
         if (orders.length === 0) {
             throw new Error(
-                `Order with the ID ${id} not found: `,
+                `Order with the id of ${id} not found `,
                 404,
             );
         }
+
         return orders;
-  } catch (error) {
-    return error.message;
-  }
+      } catch (error) {
+        return error.message;
+    }
 };
 module.exports = {
     getOrderById,
 };
-
