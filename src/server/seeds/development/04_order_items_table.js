ae80473f8c7577833a504225aee00f2af7de2657
Infamou5 <oreal_b52@hotmail.com>
2022-02-20 13:10:32 +0100

    Update

--- src/server/seeds/development/04_order_items_table.js
+++ src/server/seeds/development/04_order_items_table.js
@@ -1,56 +1,48 @@
-/* eslint-disable prefer-arrow-callback */
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('order_items')
     .del()
-    .then(()=> 
+    .then(() =>
       // Inserts seed entries
-       knex('order_items').insert([
+      knex('order_items').insert([
         {
           order_id: 1,
           product_id: 1,
-          quantity: 4,
+          quantity: 1,
+          created_at: knex.fn.now(),
+          updated_at: knex.fn.now(),
         },
         {
           order_id: 2,
           product_id: 2,
-          quantity: 1,
+          quantity: 2,
+          created_at: knex.fn.now(),
+          updated_at: knex.fn.now(),
         },
         {
           order_id: 3,
-          product_id: 7,
-          quantity: 2,
+          product_id: 3,
+          quantity: 3,
+          created_at: knex.fn.now(),
+          updated_at: knex.fn.now(),
         },
         {
           order_id: 4,
-          product_id: 20,
-          quantity: 3,
-        },
-        {
-          order_id: 1,
           product_id: 4,
-          quantity: 3,
-        },
-        {
-          order_id: 3,
-          product_id: 6,
-          quantity: 2,
-        },
-        {
-          order_id: 4,
-          product_id: 20,
-          quantity: 2,
-        },
-        {
-          order_id: 3,
-          product_id: 12,
-          quantity: 3,
-        },
-        {
-          order_id: 6,
-          product_id: 12,
           quantity: 4,
+          created_at: knex.fn.now(),
+          updated_at: knex.fn.now(),
         },
-      ])
-    );
+        {
+          order_id: 5,
+          product_id: 5,
+          quantity: 5,
+          created_at: knex.fn.now(),
+          updated_at: knex.fn.now(),
+        },
+      ]),
+    )
+    .catch((error) => {
+      console.error(error);
+    });
 };
