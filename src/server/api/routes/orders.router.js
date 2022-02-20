b73029aa9df27ce2f9c079d67e188a59c4618dcc
Natalia Sowa <natalia.sowaa@gmail.com>
2022-02-20 17:46:26 +0100

    fix errors in swagger

--- src/server/api/routes/orders.router.js
+++ src/server/api/routes/orders.router.js
@@ -5,31 +5,29 @@ const router = express.Router({ mergeParams: true });
 const ordersController = require('../controllers/orders.controller');
 /**
  * @swagger
- * /orders/{ID}:
+ * /orders/{id}:
  *  get:
  *    tags:
  *    - Orders
- *    summary: Get order by ID
+ *    summary: Get orders by order id
  *    description:
- *      Will return single order with a matching ID.
+ *      Will return all orders for specific id.
  *    produces: application/json
  *    parameters:
  *     - in: path
- *       name: ID
+ *       name: id
  *       schema:
  *         type: integer
  *         required: true
- *         description: The ID of the order to get
+ *         description: Get all orders of specific user by id
  *
  *    responses:
  *      200:
  *        description: Successful request
+ *      404:
+ *        description: incorrect entry with the provided id
  *      5XX:
  *        description: Unexpected error.
- *      400:
- *        description: Bad request. Order ID must be an integer and larger than 0.
- *      404:
- * description: A order with the specified ID was not found.
  */
 router.get('/:id', (req, res, next) => {
     ordersController
