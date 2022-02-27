fc61c49623c066697c5ad28baae1cd1cda63bbc2
Darya-s <darya.stepanova95@gmail.com>
2022-02-27 06:18:43 +0100

    Create families.controller.js

---
+++ src/server/api/controllers/families.controller.js
@@ -0,0 +1,26 @@
+const knex = require('../../config/db');
+const HttpError = require('../lib/utils/http-error');
+
+const getFamilies = async () => {
+  return knex('families');
+};
+
+const getFamilyById = async (id) => {
+  if (!id) {
+    throw new HttpError('Id should be a number', 400);
+  }
+
+  try {
+    const families = await knex('families').where({ id });
+    if (families.length === 0) {
+      throw new Error(`incorrect entry with the id of ${id}`, 404);
+    }
+    return families;
+  } catch (error) {
+    return error.message;
+  }
+};
+module.exports = {
+  getfamilies,
+  getFamilyById,
+};
