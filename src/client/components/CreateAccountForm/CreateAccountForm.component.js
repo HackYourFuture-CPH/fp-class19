867f9e2aed0ff4756b990c0b0465f3bc12df3df2
FlorentinaPetica <florentina.petica@gmail.com>
2022-02-28 16:59:01 +0100

    created form data for all inputs

--- src/client/components/CreateAccountForm/CreateAccountForm.component.js
+++ src/client/components/CreateAccountForm/CreateAccountForm.component.js
@@ -1,157 +1,152 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons';
 import './CreateAccountForm.styles.css';
 
-export default function CreateAccount({ onSubmit }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+export default function CreateAccount() {
   const [passwordShown, setPasswordShown] = useState(false);
   const [confirmPasswordShown, setConfirmPasswordShown] = useState(false);
-  const [fullName, setFullName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [address, setAddress] = useState('');
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
-  const [zipCode, setZipCode] = useState('');
+
   const handleSubmit = (e) => {
+    const formData = new FormData(e.target);
     e.preventDefault();
-    onSubmit({
-      email,
-      password,
-      passwordConfirm,
-      fullName,
-      phoneNumber,
-      address,
-      city,
-      country,
-      zipCode,
+    
+    fetch('/api/users', {
+      method: 'POST',
+      body: {
+        full_name: formData.get('full_name'),
+        email: formData.get('email'),
+        mobile: Number(formData.get('mobile')),
+        address: formData.get('address'),
+        zipcode: Number(formData.get('zipcode')),
+        city: formData.get('city'),
+        country: formData.get('country')
+      },
     });
-  };
+    console.log({
+      full_name: formData.get('full_name'),
+      email: formData.get('email'),
+      mobile: Number(formData.get('mobile')),
+      address: formData.get('address'),
+      zipcode: Number(formData.get('zipcode')),
+      city: formData.get('city'),
+      country: formData.get('country')
+    });
+}
 
   return (
     <form onSubmit={handleSubmit}>
-        <div className="create-account-container">
-          <h1>Create Account</h1>
-          <h2>Log in details</h2>
-          <label htmlFor="email">
-            Email
+      <div className="create-account-container">
+        <h1>Create Account</h1>
+        <h2>Log in details</h2>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            // value={email}
+            required
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <div className="show-hide-password-container">
             <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              type={passwordShown ? 'text' : 'password'}
+              // name="password"
+              // value={password}
               required
             />
-          </label>
-          <label htmlFor="password">
-            Password
-            <div className="show-hide-password-container">
-              <input
-                type={passwordShown ? 'text' : 'password'}
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <FontAwesomeIcon
-                onClick={() => setPasswordShown(!passwordShown)}
-                icon={passwordShown ? faEye : faEyeSlash}
-              />
-            </div>
-          </label>
-          <label htmlFor="password">
-            Confirm Password
-            <div className="show-hide-password-container">
-              <input
-                type={confirmPasswordShown ? 'text' : 'password'}
-                name="password"
-                value={passwordConfirm}
-                onChange={(e) => setPasswordConfirm(e.target.value)}
-                required
-              />
-              <FontAwesomeIcon
-                onClick={() => setConfirmPasswordShown(!confirmPasswordShown)}
-                icon={confirmPasswordShown ? faEye : faEyeSlash}
-              />
-            </div>
-          </label>
-        </div>
-        <div className="create-account-container">
-          <h2>Contact details</h2>
-          <label htmlFor="fullName">
-            Full name
+            <FontAwesomeIcon
+              onClick={() => setPasswordShown(!passwordShown)}
+              icon={passwordShown ? faEye : faEyeSlash}
+            />
+          </div>
+        </label>
+        <label htmlFor="password">
+          Confirm Password
+          <div className="show-hide-password-container">
             <input
-              type="text"
-              name="fullName"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
+              type={confirmPasswordShown ? 'text' : 'password'}
+              // name="passwordConfirm"
+              // value={passwordConfirm}
               required
             />
-          </label>
-          <label htmlFor="phone">
-            Mobile
-            <input
-              type="tel"
-              name="phone"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              required
+            <FontAwesomeIcon
+              onClick={() => setConfirmPasswordShown(!confirmPasswordShown)}
+              icon={confirmPasswordShown ? faEye : faEyeSlash}
             />
-          </label>
-          <h2>Delivery info</h2>
-          <label htmlFor="address">
-            Address
-            <input
-              type="text"
-              name="address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              required
-            />
-          </label>
-          <label htmlFor="city">
-            City
-            <input
-              type="text"
-              name="city"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              required
-            />
-          </label>
-          <label htmlFor="country">
-            Country
-            <input
-              type="text"
-              name="country"
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-              required
-            />
-          </label>
-          <label htmlFor="zipcode">
-            Zip-code
-            <input
-              type="text"
-              pattern="\d*/"
-              title="Only numbers allowed"
-              name="zipcode"
-              value={zipCode}
-              onChange={(e) => setZipCode(e.target.value)}
-              required
-            />
-          </label>
-          <button className="create-account-button" type="submit">
-            Create account
-          </button>
-        </div>
-      </form>
+          </div>
+        </label>
+      </div>
+      <div className="create-account-container">
+        <h2>Contact details</h2>
+        <label htmlFor="fullName">
+          Full name
+          <input
+            type="text"
+            name="full_name"
+            // value={full_name}
+            required
+          />
+        </label>
+        <label htmlFor="mobile">
+          Mobile
+          <input
+            type="tel"
+            name="mobile"
+            // value={phoneNumber}
+            required
+          />
+        </label>
+        <h2>Delivery info</h2>
+        <label htmlFor="address">
+          Address
+          <input
+            type="text"
+            name="address"
+            // value={address}
+            required
+          />
+        </label>
+        <label htmlFor="city">
+          City
+          <input
+            type="text"
+            name="city"
+            // value={city}
+            required
+          />
+        </label>
+        <label htmlFor="country">
+          Country
+          <input
+            type="text"
+            name="country"
+            // value={country}
+            required
+          />
+        </label>
+        <label htmlFor="zipcode">
+          Zip-code
+          <input
+            type="text"
+            // pattern="\d*/"
+            title="Only numbers allowed"
+            name="zipcode"
+            // value={zipCode}
+            required
+          />
+        </label>
+        <button className="create-account-button" type="submit">
+          Create account
+        </button>
+      </div>
+    </form>
   );
 }
 
-CreateAccount.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
+// CreateAccount.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
