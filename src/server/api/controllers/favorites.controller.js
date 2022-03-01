bb34458b3b39eb7e713ff77d88dea56e14aa698e
Natalia Sowa <natalia.sowaa@gmail.com>
2022-03-01 14:28:07 +0100

    corrected the response status codes

--- src/server/api/controllers/favorites.controller.js
+++ src/server/api/controllers/favorites.controller.js
@@ -1,27 +1,36 @@
 const knex = require('../../config/db');
 const moment = require('moment-timezone');
+const HttpError = require('../lib/utils/http-error');
 
-const addToFavorites = async (body) => {
-  console.log('in add');
-  await knex('favorites').insert({
-    user_id: body.user_id,
-    product_id: body.product_id,
-    created_at: moment(Date.now()).format(),
-  });
+const addToFavorites = async(body) => {
+    console.log('in add');
+    await knex('favorites').insert({
+        user_id: body.user_id,
+        product_id: body.product_id,
+        created_at: moment(Date.now()).format(),
+    });
 
-  return {
-    successful: true,
-  };
+    return {
+        successful: true,
+    };
 };
 
-const deleteFromFavorites = async (userId, productId) => {
-  console.log('delete');
-  return knex('favorites')
-    .where({ user_id: userId, product_id: productId })
-    .del();
+const deleteFromFavorites = async(userId, productId) => {
+    const reg = /^\d+$/;
+    const id = parseInt(productId, 10);
+    if (isNaN(id) || !reg.test(productId) || id < 1) {
+        throw new HttpError(`Bad request. Id should be a number`, 400);
+    }
+
+    const favoriteToDelete = await knex('favorites')
+        .where({ user_id: userId, product_id: id }).del();
+    if (favoriteToDelete.length === 0) {
+        throw new HttpError(`Bad request. userID or productID doesn't exist`, 404);
+    }
+
 };
 
 module.exports = {
-  deleteFromFavorites,
-  addToFavorites,
+    deleteFromFavorites,
+    addToFavorites,
 };
