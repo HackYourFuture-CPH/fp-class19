7eeed91d961e05f162e030d15ba58f8d4ecb921a
suvarna <suvarna.akkala@gmail.com>
2022-03-05 12:04:57 +0100

    build error in favorites page

--- src/client/containers/FavoritePage/FavoritePage.Container.js
+++ src/client/containers/FavoritePage/FavoritePage.Container.js
@@ -1,41 +1,32 @@
-
 import './FavoritePage.styles.css';
 
-   
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 import FavouriteList from '../../components/FavouriteList/FavouriteList.component';
 
+export const addProductToFavorites = (userId, productId) => {
+  console.log('in add favorites container');
+  console.log(userId, productId);
 
-
-export const addProductToFavorites = (user_id,product_id) => {
-  console.log("in add favorites container");
-  console.log(user_id,product_id)
- 
   const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        product_id:product_id,
-        user_id: user_id
-        
-       }),
-       
-    };
-    fetch("/api/favorites/add", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-      });
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      product_id: productId,
+      user_id: userId,
+    }),
   };
+  fetch('/api/favorites/add', requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    });
+};
 
-export default function FavoritePage  () {
-  
-  
+export default function FavoritePage() {
   const [favorites, setFavorites] = useState([]);
 
-
   const GetFavorites = useCallback(() => {
-    let apiUrl = "/api/users/2/favorites";
+    let apiUrl = '/api/users/2/favorites';
     console.log(apiUrl);
     fetch(apiUrl)
       .then((res) => res.json())
@@ -43,20 +34,16 @@ export default function FavoritePage  () {
         console.log(result);
 
         if (result) {
-          for (let i=0;i<result.length;i++){
-            result[i].discount=result[i].discount_percent
-            result[i].image=result[i].picture
-            result[i].currency='DKK'
-
+          for (let i = 0; i < result.length; i++) {
+            result[i].discount = result[i].discount_percent;
+            result[i].image = result[i].picture;
+            result[i].currency = 'DKK';
           }
           const items = result.map((item) => item);
           setFavorites((prev) => {
             return prev.concat(items);
-          }
-          );
+          });
           console.log(favorites);
-
-          
         }
       });
   }, []);
@@ -66,13 +53,9 @@ export default function FavoritePage  () {
   }, [GetFavorites]);
 
   return (
-    <div className='favoritelist'>
-      <h2 className='fav-heading'>Favorites</h2>
-      <FavouriteList favorites={favorites} setFavorites={setFavorites}/>
+    <div className="favoritelist">
+      <h2 className="fav-heading">Favorites</h2>
+      <FavouriteList favorites={favorites} setFavorites={setFavorites} />
     </div>
   );
-};
-
-
-
-
+}
