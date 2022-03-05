0b3aa1892a3a048d2af35f6f3e01e7b8932cd41c
shabnam-ght <www.shabnamght77@gmail.com>
2022-03-05 08:26:45 +0100

    add produc preview component

--- src/client/components/ProductPreview/ProductPreview.component.js
+++ src/client/components/ProductPreview/ProductPreview.component.js
@@ -1,72 +1,9 @@
-// import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
-// import Button from '../Button/Button.component';
-// import './ProductPreview.css';
-
-// export default function ProductPreview({
-//   image,
-//   name,
-//   price,
-//   size,
-//   currency,
-//   onClick,
-// }) {
-//   const [count, setCount] = useState(0);
-
-//   function decreamentCount() {
-//     setCount((prevCount) => prevCount - 1);
-//   }
-
-//   function increamentCount() {
-//     setCount((prevCount) => prevCount + 1);
-//   }
-//   return (
-//     <div>
-//       <div className="product-detail-container">
-//         <div className="product-preview-content">
-//           <img src={image} alt={name} />
-//           <div className="product-preview-details">
-//             <h3 className="product-name">{name}</h3>
-//             <div className="gray-container">
-//               <span>
-//                 {' '}
-//                 <p>{size}</p>
-//               </span>
-
-//               <span>
-//                 <p>{price}</p>
-//               </span>
-//               <p>Quantity</p>
-//               <button onClick={decreamentCount}>-</button>
-//               <span>{count}</span>
-//               <button onClick={increamentCount}>+</button>
-//               <Button primary="true" label="ADD"></Button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-// ProductPreview.propTypes = {
-//   image: PropTypes.string.isRequired,
-//   name: PropTypes.string.isRequired,
-//   price: PropTypes.number.isRequired,
-//   size: PropTypes.string.isRequired,
-//   currency: PropTypes.string,
-//   onClick: PropTypes.func,
-// };
-// ProductPreview.defaultProps = {
-//   onClick: null,
-//   currency: 'DKK',
-// };
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import './ProductDetail.styles.css';
+import './ProductPreview.styles.css';
 import Button from '../Button/Button.component';
 
-export default function ProductDetail({
+export default function ProductPreview({
   product,
   quantityCount,
   setQuantityCount,
@@ -86,8 +23,8 @@ export default function ProductDetail({
 
   const { name, price, picture } = product;
   return (
-    <div className="productDetail">
-      <article className="productDetail-container">
+    <div className="productPreview">
+      <article className="productPreview-container">
         <section className="image-container">
           <img src={picture} alt={name} />
         </section>
@@ -122,7 +59,7 @@ export default function ProductDetail({
             </div>
           </div>
           <Button
-            className="productDetail-button"
+            className="productPreview-button"
             type="button"
             onClick={' '}
             primary={true}
@@ -134,7 +71,7 @@ export default function ProductDetail({
   );
 }
 
-ProductDetail.propTypes = {
+ProductPreview.propTypes = {
   product: PropTypes.shape({
     name: PropTypes.string,
     price: PropTypes.number,
