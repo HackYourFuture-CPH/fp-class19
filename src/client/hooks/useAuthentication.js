ed6d8565d15596243d210de9a8d77e6c06b8ff53
Divyajg <guttuladivyajyothi@gmail.com>
2022-03-06 13:08:17 +0100

    updated profile path to user-profile path

--- src/client/hooks/useAuthentication.js
+++ src/client/hooks/useAuthentication.js
@@ -4,53 +4,53 @@ import { getApps } from 'firebase/app';
 import { useFirebase } from '../firebase/FirebaseContext';
 
 function authRedirect() {
-  if (
-    window.location.pathname === '/sign-in' ||
-    window.location.pathname === '/'
-  ) {
-    window.location.href = '/profile';
-  }
+    if (
+        window.location.pathname === '/sign-in' ||
+        window.location.pathname === '/'
+    ) {
+        window.location.href = '/user-profile';
+    }
 }
 
 /**
  * Docs: https://firebase.google.com/docs/auth/web/start#set_an_authentication_state_observer_and_get_user_data
  */
 export function useAuthentication() {
-  const [authState, setAuthState] = useState({
-    isAuthenticated: false,
-    isLoading: true,
-    user: null,
-  });
-
-  const { isInitialized } = useFirebase();
-
-  const firebaseInitialized = getApps().length > 0;
-
-  useEffect(() => {
-    if (!firebaseInitialized) {
-      return;
-    }
-
-    const auth = getAuth();
-
-    auth.onAuthStateChanged((user) => {
-      // if user exists it means authenticated
-      if (user) {
-        setAuthState({
-          isAuthenticated: true,
-          isLoading: false,
-          user,
-        });
-        authRedirect();
-      } else {
-        setAuthState({
-          isAuthenticated: false,
-          isLoading: false,
-          user: null,
-        });
-      }
+    const [authState, setAuthState] = useState({
+        isAuthenticated: false,
+        isLoading: true,
+        user: null,
     });
-  }, [isInitialized, firebaseInitialized]);
 
-  return { ...authState };
+    const { isInitialized } = useFirebase();
+
+    const firebaseInitialized = getApps().length > 0;
+
+    useEffect(() => {
+        if (!firebaseInitialized) {
+            return;
+        }
+
+        const auth = getAuth();
+
+        auth.onAuthStateChanged((user) => {
+            // if user exists it means authenticated
+            if (user) {
+                setAuthState({
+                    isAuthenticated: true,
+                    isLoading: false,
+                    user,
+                });
+                authRedirect();
+            } else {
+                setAuthState({
+                    isAuthenticated: false,
+                    isLoading: false,
+                    user: null,
+                });
+            }
+        });
+    }, [isInitialized, firebaseInitialized]);
+
+    return {...authState };
 }
