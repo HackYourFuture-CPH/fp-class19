784cddf3926d2525445c60024d36ceb079dbec38
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2022-03-08 15:50:24 +0100

    Paypal component storie addedd

---
+++ src/client/components/Paypal/Paypal.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Paypal from './Paypal.component';
+
+export default {
+  title: 'Components / Paypal',
+  component: Paypal,
+};
+
+export function PaypalButton(args) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Paypal {...args} />;
+}
+
+PaypalButton.args = {
+  orderId: 'orderId',
+  totalAmount: 100,
+  userName: 'john doe',
+};
