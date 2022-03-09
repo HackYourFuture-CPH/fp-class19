25755a252edfc737fc2243552e78f701408150dc
suvarna <suvarna.akkala@gmail.com>
2022-03-09 20:41:02 +0100

    changed component

--- src/client/components/OrdersSummary/OrderComponent.js
+++ src/client/components/OrdersSummary/OrderComponent.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import './OrdersSummary.styles.css';
 
-export default function OrderComponent({ order, orderDetails}) {
+export default function OrderComponent({ order, details }) {
+  
   const [fullView, setFullView] = React.useState(false);
   const viewOrder = () => {
     setFullView(!fullView);
   };
-  React.useEffect(() => {
-    setCurrentRange(propProducts.slice(0, args.productsPerPage));
-  }, [propProducts, args.productsPerPage]);
+
   return (
-      <div>
-            <li key={order.id}>
-              <div>
-                <p>Order No: {order.id} ({order.nr_of_items})
-                <span>Status: {order.status}</span>
-                <span>Created on: {order.updated_at}</span>
-                <span>Kr. {order.total_amount}</span>
-                <button onClick={viewOrder}>{fullView ? <p>&#x25BC;</p>:<p>&#x25B2;</p>}</button></p> 
-              </div>
-              {fullView && <>
-                {orderDetails.filter(odr=>odr.id==order.id)}
-                <img src={orderDetails.filter(odr=>odr.id==order.id).picture}/>
-                <p>{orderDetails.filter(odr=>odr.id==order.id).name}</p>
-                <p>Quantity: {orderDetails.filter(odr=>odr.id==order.id).quantity}</p>
-              </>}
-            </li>
-      </div>
+    <div>
+      <li key={order.order_number}>
+        <div>
+          <p>
+            Order No: {order.order_number} ({order.nr_of_items}items)
+            <span>{order.updated_at.slice(0,10)}</span>
+            <span>Kr. {order.total_amount}</span>
+            <button onClick={viewOrder}>
+              {fullView ? <p>&#x25BC;</p> : <p>&#x25B2;</p>}
+            </button>
+          </p>
+        </div>
+        {fullView && (
+          <>
+            {details && <>
+            <img
+              src={details.picture}
+            />
+            <p>{details.name}</p>
+            <p>
+              Quantity:{' '}
+              {details.quantity}
+            </p></>}
+          </>
+        )}
+      </li>
+    </div>
   );
 }
--- src/client/components/OrdersSummary/OrdersSummary.component.js
+++ src/client/components/OrdersSummary/OrdersSummary.component.js
@@ -3,18 +3,15 @@ import OrderComponent from './OrderComponent';
 import './OrdersSummary.styles.css';
 
 
-export default function OrdersSummary({ orders }) {
-  const [fullView, setFullView] = React.useState(false);
-  const viewOrder = () => {
-    setFullView(!fullView);
-  };
+export default function OrdersSummary({ orders, orderDetails }) {
+  
   return (
     <div className="orders-view">
       <div>
         <ul className="order-list">
           {orders.map((order) => (
             <li key={order.id} className="order-item">
-              <OrderComponent order={order}/>
+              <OrderComponent order={order} details={orderDetails.filter((odr) => odr.id == order.id)}/>
             </li>
           ))}
         </ul>
