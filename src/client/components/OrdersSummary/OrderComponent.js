ef4e4dc9fdbdcb4c75a292234253443dcc38692a
suvarna <suvarna.akkala@gmail.com>
2022-03-10 14:21:01 +0100

    import

--- src/client/components/OrdersSummary/OrderComponent.js
+++ src/client/components/OrdersSummary/OrderComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './OrdersSummary.styles.css';
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 export default function OrderComponent({ order, details }) {
   const [fullView, setFullView] = React.useState(false);
