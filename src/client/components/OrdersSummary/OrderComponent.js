52a4268b56815a7c1b82cb92f6d6d5f605574540
suvarna <suvarna.akkala@gmail.com>
2022-03-10 11:58:32 +0100

    fixed moment instead of slice

--- src/client/components/OrdersSummary/OrderComponent.js
+++ src/client/components/OrdersSummary/OrderComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './OrdersSummary.styles.css';
+import moment from 'moment';
 
 export default function OrderComponent({ order, details }) {
   const [fullView, setFullView] = React.useState(false);
@@ -10,21 +11,35 @@ export default function OrderComponent({ order, details }) {
   return (
     <div>
       <li>
-        <div className='flex-container'>
-          <div><h3>
-            Order No: {order.order_number}<span> ({order.nr_of_items} items)</span></h3></div>
-            <div><p>{order.updated_at.slice(0, 10)}</p>
-            <p>Kr. {order.total_amount}</p></div>
-            <div> <button type='button' onClick={viewOrder}>
+        <div className="flex-container">
+          <div>
+            <h3>
+              Order No: {order.order_number}
+              <span> ({order.nr_of_items} items)</span>
+            </h3>
+          </div>
+          <div>
+            <p>{moment(order.updated_at).format('DD-MM-YYYY')}</p>
+            <p>Kr. {order.total_amount}</p>
+          </div>
+          <div>
+            {' '}
+            <button type="button" onClick={viewOrder}>
               {fullView ? <p>&#x25BC;</p> : <p>&#x25B2;</p>}
             </button>
-            </div>
+          </div>
         </div>
-        {fullView && <div className='flex-container flex-container2'>
-                <div><img src={details[0].picture} alt='orderedItem'/></div>
-                <div><p>{details[0].name}</p>
-                <p>Quantity: {details[0].quantity}</p></div>
-              </div>}
+        {fullView && (
+          <div className="flex-container flex-container2">
+            <div>
+              <img src={details[0].picture} alt="orderedItem" />
+            </div>
+            <div>
+              <p>{details[0].name}</p>
+              <p>Quantity: {details[0].quantity}</p>
+            </div>
+          </div>
+        )}
       </li>
     </div>
   );
