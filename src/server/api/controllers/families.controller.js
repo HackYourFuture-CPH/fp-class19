a6bd7c3e4808fc277b86542b1ea7257694b429fa
saloumeh-67 <s.saaraabi@gmail.com>
2022-03-13 13:18:50 +0100

    make our Id as string and fixing error message

--- src/server/api/controllers/families.controller.js
+++ src/server/api/controllers/families.controller.js
@@ -3,20 +3,23 @@ const HttpError = require('../lib/utils/http-error');
 
 const getFamilies = async () => knex('families');
 
-
-const getFamilyById = async (id) => {
-  if (!id) {
+const getFamilyById = async (idStr) => {
+  const id = parseInt(idStr.trim(), 10);
+  if (isNaN(id) || id < 1) {
     throw new HttpError('Id should be a number', 400);
   }
 
   try {
     const families = await knex('families').where({ id });
     if (families.length === 0) {
-      throw new Error(`incorrect entry with the id of ${id}`, 404);
+      throw new HttpError(`incorrect entry with the id of ${id}`, 404);
     }
     return families;
   } catch (error) {
-    return error.message;
+    if (error instanceof HttpError) {
+      throw error;
+    }
+    throw new HttpError('Something went wrong', 500);
   }
 };
 module.exports = {
