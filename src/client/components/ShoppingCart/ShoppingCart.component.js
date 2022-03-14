4bc5920778aba419559c724d3fd683c164ac5bbd
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2022-03-14 19:58:09 +0100

    paypal button component  to shoping cart

--- src/client/components/ShoppingCart/ShoppingCart.component.js
+++ src/client/components/ShoppingCart/ShoppingCart.component.js
@@ -5,12 +5,14 @@ import './ShoppingCart.styles.css';
 import ShoppingCartImage from './ShoppingCartImage';
 import ModifyProductQuantity from './ModifyProductQuantity';
 import RemoveFromShoppingCart from './RemoveFromShoppingCart';
+import Paypal from '../Paypal/Paypal.component';
 
 export default function ShoppingCart(props) {
   const { shoppingCart, setShoppingCart, userDetail } = props;
   const [totalPrice, setTotalPrice] = useState(getTotalPrice());
   const [totalDiscount, setTotalDiscount] = useState(getTotalDiscount());
   const [totalPayment, setTotalPayment] = useState(getTotalPayment());
+  const [checkout, setCheckout] = useState(false);
 
   function getTotalPrice() {
     const totalPriceOfProducts = shoppingCart.reduce(
@@ -90,9 +92,17 @@ export default function ShoppingCart(props) {
             </div>
 
             <div>
-              <button type="button" className="proceed-payment">
-                Proceed to payment
-              </button>
+              {checkout ? (
+                <Paypal />
+              ) : (
+                <button
+                  type="button"
+                  className="proceed-payment"
+                  onClick={() => setCheckout(true)}
+                >
+                  Proceed to payment
+                </button>
+              )}
             </div>
           </div>
           <div>
